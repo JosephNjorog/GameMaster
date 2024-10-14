aba32ef14fcb3ef14bb5164b726b6192456f936e
JosephNjorog <njojoe11173@gmail.com>
2024-10-14 13:43:02 +0300

    Add game routes for creating, getting, joining, and starting a game

---
+++ backend/routes/gameRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const gameController = require('../controllers/gameController');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/create', protect, gameController.createGame);
+router.get('/:id', protect, gameController.getGame);
+router.post('/:id/join', protect, gameController.joinGame);
+router.post('/:id/start', protect, gameController.startGame);
+
+module.exports = router;
